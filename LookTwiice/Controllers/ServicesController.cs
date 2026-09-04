8781c8d1d30bc1712d2a4c0a4f540d0ebce959c0
Zekeriyya Yunus Sönmez <zyunussonmez@gmail.com>
2026-09-04 12:27:14 +0300

    Services sayfası tasarlandı.

---
+++ LookTwiice/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LookTwiice.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
