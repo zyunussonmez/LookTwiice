aede31819e1c91eb70b053982cd4ab83c058b902
Zekeriyya Yunus Sönmez <zyunussonmez@gmail.com>
2026-09-04 11:45:24 +0300

    Home sayfası güncellendi ve About sayfası tasarlandı.

---
+++ LookTwiice/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LookTwiice.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
